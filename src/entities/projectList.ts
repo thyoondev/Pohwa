f7f1f67c05f1233987421f8dcb95ee4f90d817c6
Taehee Yoon <th.yoon.dev@gmail.com>
2024-09-26 14:43:23 +0900

    feat : 작업물 추가

--- src/entities/projectList.ts
+++ src/entities/projectList.ts
@@ -1,14 +1,32 @@
 export const PROJECT_LIST = [
   {
+    id: 1,
+    title: "Korea Motorcycle Film Project Chapter .01",
+    description: "@ezezho_bike / 원낭만",
+    videoUrl: "https://pohwa.xyz/assets/video/main/powha_video_pj1.mp4",
+  },
+  {
+    id: 2,
+    title: "Korea Motorcycle Film Project Chapter .02",
+    description: "@arr.bike / 아르르",
+    videoUrl: "https://pohwa.xyz/assets/video/main/powha_video_pj2.mp4",
+  },
+  {
     id: 3,
     title: "Korea Motorcycle Film Project Chapter .03",
     description: "@jh_etyisd / 임정현",
-    videoUrl: "https://pohwa.xyz/assets/video/main/powha_video_2.mp4",
+    videoUrl: "https://pohwa.xyz/assets/video/main/powha_video_pj3.mp4",
+  },
+  {
+    id: 4,
+    title: "Korea Motorcycle Film Project Chapter .04",
+    description: "@mincho__o / 민초",
+    videoUrl: "https://pohwa.xyz/assets/video/main/powha_video_pj4.mp4",
   },
   {
     id: 5,
     title: "Korea Motorcycle Film Project Chapter .05",
     description: "@rider_rim321 / 하루",
-    videoUrl: "https://pohwa.xyz/assets/video/main/powha_video_1.mp4",
+    videoUrl: "https://pohwa.xyz/assets/video/main/powha_video_pj5.mp4",
   },
 ];
